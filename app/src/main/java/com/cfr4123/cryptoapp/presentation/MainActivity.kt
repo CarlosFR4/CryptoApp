4cc7b64a7288ebeec29c4cbfef61417eb86b1c9c
CarlosFR4 <cfr4123@gmail.com>
2021-10-07 01:25:12 -0500

    compose dependencies, and setup

--- app/src/main/java/com/cfr4123/cryptoapp/presentation/MainActivity.kt
+++ app/src/main/java/com/cfr4123/cryptoapp/presentation/MainActivity.kt
@@ -2,11 +2,22 @@ package com.cfr4123.cryptoapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import com.cfr4123.cryptoapp.R
+import com.cfr4123.cryptoapp.presentation.ui.CryptoAppTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            CryptoAppTheme {
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
     }
 }
